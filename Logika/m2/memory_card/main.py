78f75ef2b3e69b4ebcf660d4fca56d9b3bca121b
lukoil1 <141442838+lukoil1@users.noreply.github.com>
2023-09-28 22:06:05 +0300

    Add files via upload

---
+++ Logika/m2/memory_card/main.py
@@ -0,0 +1,8 @@
+from random import choice, shuffle
+from time import sleep
+from menu_window import*
+from main_window import*
+
+
+main_win.show()
+app.exec()
---
+++ Logika/m2/memory_card/main_window.py
@@ -0,0 +1,65 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+main_win = QWidget()
+main_win.resize(600, 500)
+
+btn_menu = QPushButton('Меню')
+btn_rest = QPushButton('Відпочити')
+btn_ok = QPushButton('Відповісти')
+
+box_minuts = QSpinBox()
+box_minuts.setValue(10)
+
+lb_questien = QLabel('')
+
+
+RadioGroupBox = QGroupBox('Варінти відповідей')
+RadioGroup = QButtonGroup()
+
+r_btn1 = QRadioButton('')
+r_btn2 = QRadioButton('')
+r_btn3 = QRadioButton('')
+r_btn4 = QRadioButton('')
+
+RadioGroup.addButton(r_btn1)
+RadioGroup.addButton(r_btn2)
+RadioGroup.addButton(r_btn3)
+RadioGroup.addButton(r_btn4)
+
+Layout_ans1 =QHBoxLayout() 
+Layout_ans2 =QVBoxLayout() 
+Layout_ans3 =QVBoxLayout() 
+
+Layout_ans2.addWidget(r_btn1)
+Layout_ans2.addWidget(r_btn2)
+
+Layout_ans3.addWidget(r_btn3)
+Layout_ans3.addWidget(r_btn4)
+
+Layout_ans1.addLayout(Layout_ans2)
+Layout_ans1.addLayout(Layout_ans3)
+
+RadioGroupBox.setLayout(Layout_ans1)
+
+AnsGroupBox =QGroupBox('Результат тесту')
+
+lb_correct = QLabel('')
+lb_result = QLabel('')
+
+Layout_res =QVBoxLayout()
+Layout_res.addWidget(lb_result , alignment=(Qt.AlignLeft|Qt.AlignTop))
+Layout_res.addWidget(lb_correct , alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(Layout_res)
+AnsGroupBox.hide()
+
+Layout_line1 = QHBoxLayout()
+Layout_line2 = QHBoxLayout()
+Layout_line3 = QHBoxLayout()
+Layout_line4 = QHBoxLayout()
+
+Layout_line1.addWidget(btn_menu)
+Layout_line1.addStretch(1)
+Layout_line1.addWidget(btn_rest)
+Layout_line1.addWidget(box_minuts)
+Layout_line1.addWidget(QLabel('хвилин'))
