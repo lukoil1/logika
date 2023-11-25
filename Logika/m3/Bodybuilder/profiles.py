3df58a0698cc2c9cc830cb3005d27dc02deeea34
lukoil1 <141442838+lukoil1@users.noreply.github.com>
2023-11-25 13:38:17 +0300

    Add files via upload

--- Logika/m3/Bodybuilder/profiles.py
+++ Logika/m3/Bodybuilder/profiles.py
@@ -2,6 +2,17 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 import json
 
+def read_file():
+    global profiles
+    with open('m3/Bodybuilder/data_profiles.json', 'r', encoding='utf8') as file:
+        profiles = json.load(file)
+read_file()
+
+def write_file():
+    with open('m3/Bodybuilder/data_profiles.json', 'w', encoding='utf8') as file:
+        json.dump(profiles, file , sort_keys=True, ensure_ascii=False, indent=4)    
+
+
 app = QApplication([])
 
 win_lst_profiles = QWidget()
@@ -12,6 +23,7 @@ btn_add_profile = QPushButton("Створити профіль")
 btn_delete_profile = QPushButton("Видалити профіль")
 btn_setting = QPushButton("⚙️")
 lst_profile = QListWidget()
+lst_profile.addItems(profiles)
 lb_name = QLabel("Ім'я:")
 lb_gender = QLabel("Стать:")
 lb_age = QLabel("Вік:")
@@ -47,4 +59,22 @@ col2.addWidget(field_goal)
 col2.addWidget(lb_notes)
 col2.addWidget(field_notes)
 
+def show_info():
+    global key
+    key = lst_profile.currentItem().text()
+    lb_name.setText("Ім'я: " +profiles[key]["personal"]["name"])
+    lb_gender.setText("Стать: " +profiles[key]["personal"]["gender"])
+    lb_age.setText("Вік: " +str(profiles[key]["measurments"]["age"]))
+    
+def edit_goal():
+    profiles[key]["goal"] = field_goal.text()
+    write_file()
+
+def edit_notes():
+    profiles[key]["notes"] = field_notes.toPlainText()
+    write_file()   
+    
+lst_profile.itemClicked.connect(show_info)
+field_goal.editingFinished.connect(edit_goal)
+field_notes.textChanged.connect(edit_notes)
 win_lst_profiles.setLayout(main_layout)
