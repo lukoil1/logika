bf34e6c48c26e24aff88b59da38df39c14b0f474
lukoil1 <141442838+lukoil1@users.noreply.github.com>
2023-12-07 23:29:31 +0300

    Add files via upload

--- Logika/m3/Bodybuilder/main.py
+++ Logika/m3/Bodybuilder/main.py
@@ -9,7 +9,19 @@ def show_change():
     key = lst_profile.currentItem().text()
     fld_name.setText(profiles[key]["personal"]['name'])
     fld_height.setText(str(profiles[key]["measurments"]['height']))
-    
+    fld_weight.setText(str(profiles[key]["measurments"]["weight"]))
+    fld_age.setText(str(profiles[key]["measurments"]["age"]))
+    fld_gender.setText(str(profiles[key]["personal"]["gender"]))
+    fld_cretine.setText(str(profiles[key]["nutrition"]["creatine"]))
+    fld_protein.setText(str(profiles[key]["nutrition"]["protein"]))
+    fld_biceps.setText(str(profiles[key]["measurments"]["biceps"]))
+    fld_BMI.setText(str(profiles[key]["measurments"]["bmi"]))
+    fld_calf.setText(str(profiles[key]["measurments"]["calf"]))
+    fld_chest.setText(str(profiles[key]["measurments"]["chest"]))
+    fld_hips.setText(str(profiles[key]["measurments"]["hips"]))
+    fld_neck.setText(str(profiles[key]["measurments"]["neck"]))
+    fld_forearm.setText(str(profiles[key]["measurments"]["forearm"]))
+    fld_waist.setText(str(profiles[key]["measurments"]["waist"]))
     win_info.show()
 
 
