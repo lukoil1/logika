7345d28c7c08bcc5a05b2b35fdbfea500421deaa
Lukian <lukyanshak@gmail.com>
2024-02-24 12:58:52 +0200

    створили спрайт2 і керування до нього

--- Logika/m5/u1/catch_up.py
+++ Logika/m5/u1/catch_up.py
@@ -8,6 +8,10 @@ sprite1= image.load("Logika/m5/u1/sprite1.png")
 sprite1 = transform.scale(sprite1 , [100,100])
 y1 = 350
 x1 = 100
+sprite2 = image.load("Logika/m5/u1/sprite2.png")
+sprite2 = transform.scale(sprite2 , [100,100])
+y2 = 350
+x2 = 300  
 clock = time.Clock()
 FPS = 60
 running=True
@@ -18,7 +22,7 @@ while running:
             quit()
     window.blit(background, [0,0])
     window.blit(sprite1,[x1,y1])
-    
+    window.blit(sprite2,[x2,y2])
     keys = key.get_pressed()
     if keys[K_UP]:
         y1 -= 10
@@ -28,6 +32,15 @@ while running:
         x1 -= 10
     if keys[K_RIGHT]:
         x1 += 10
+    if keys[K_w]:
+        y2 -= 10
+    if keys[K_s]:
+        y2 += 10
+    if keys[K_a]:
+        x2 -= 10
+    if keys[K_d]:
+        x2 += 10
+        
     #K_w
     #K_a
     #K_d
