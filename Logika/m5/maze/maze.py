4603a2430d8998cc46840c3cc8624a47adcae404
Lukian <lukyanshak@gmail.com>
2024-03-02 12:33:08 +0200

    створив задній фон і почав робити клас GameSprite

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -4,3 +4,32 @@ from pygame.sprite import collide_rect
 from pygame.transform import scale, flip
 from pygame.image import load
 from random import randint
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, image, x, y, speed):
+        super().__init__()
+        self.image = load(image)
+        self.image = scale(self.image, [60,60])
+        self.rect = self.image.get_rect()
+mixer.init()
+mixer.music.load("Logika/m5/maze/jungles.ogg")
+mixer.music.play(-1)
+
+
+width = 700
+height = 500
+window = display.set_mode([width,height])
+background = load("Logika/m5/maze/background.jpg")
+background = scale(background, [width,height])
+clock = time.Clock()
+FPS = 60
+game = True
+while game == True:
+    for e in event.get():
+        if e.type== QUIT:
+            game = False
+
+
+    window.blit(background,[0,0])
+    display.update()
+    clock.tick(60)
